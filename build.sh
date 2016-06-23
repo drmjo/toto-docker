8d68bf86dec4a86f76e921b99f4652d8cd69411d
Majan Paul <majan@Majans-MacBook-Pro.local>
2016-06-23 15:40:27 -0700

    remove exposed ports of the container and change the config

--- build.sh
+++ build.sh
@@ -1,7 +1,10 @@
 set -x
 
 rm -rf symfony
-
 git clone https://github.com/drmjo/toto.git symfony
+
+export SECRET=`cat /dev/urandom | base64 | head -c 40`
+
+sed -i '.original' -e "s|{{secret}}|${SECRET}|g" symfony/app/config/parameters.yml.dist
 
 docker-compose build
--- run.sh
+++ run.sh
@@ -1,5 +1,3 @@
 set -x
 
-export SYMFONY__SESSION__SECRET=`cat /dev/urandom | base64 | head -c 40`
-
 docker-compose up &
