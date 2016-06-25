c23b7871ccb4ebd71c4349c1c497c3af67a670f1
Majan Paul <majan@Majans-MacBook-Pro.local>
2016-06-24 19:44:28 -0700

    set the user of the files

--- php-fpm/run.sh
+++ php-fpm/run.sh
@@ -1,14 +1,22 @@
+set -x
+
+cd /var/www/symfony
+
 php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
 php composer-setup.php
 php -r "unlink('composer-setup.php');"
 
 chmod 744 composer.phar
-mv composer.phar /usr/bin/composer
 
-cd /var/www/symfony
-composer install
+# su
+# mv composer.phar /usr/bin/composer
+# su -l www-data
+
+./composer.phar install
 
 php app/console doctrine:migrations:migrate --no-interaction
 php app/console doctrine:fixtures:load --no-interaction
+
+chown -R root:www-data .
 
 php5-fpm -F
